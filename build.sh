51a281667ce5c2c86500b738d4326776c6979496
Stephan Fudeus <sfudeus@users.noreply.github.com>
2021-08-28 10:44:28 +0200

    Add arm64

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ set -eu
 PRG_VERSION=$(git describe --tags)
 IMAGE_VERSION=${PRG_VERSION}
 
-docker buildx build --platform linux/amd64 --platform linux/arm/v7 -t "sfudeus/mailrelay:${IMAGE_VERSION}" --push .
+docker buildx build --platform linux/amd64 --platform linux/arm/v7 --platform linux/arm64 -t "sfudeus/mailrelay:${IMAGE_VERSION}" --push .
